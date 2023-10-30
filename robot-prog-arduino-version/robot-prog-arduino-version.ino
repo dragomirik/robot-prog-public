32a09a5ad39aa2b1ef75fd5ff988c511560fed8c
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2023-10-30 12:25:44 +0100

    ajout globals début classes et début rebond range

--- robot-prog-arduino-version/robot-prog-arduino-version.ino
+++ robot-prog-arduino-version/robot-prog-arduino-version.ino
@@ -2,48 +2,117 @@
 
 //TODO: changer le code en multi-fichiers en utilisant des namespace pour contourner les barrières du .ino ou séparer directement en .hpp et .cpp si cela fonctionne
 
+class Vector2 {
+  public:
+  Vector2(float x, float y) : _x(x), _y(y) {}
+
+  float x() const {
+    return _x;
+  }
+  float y() const {
+    return _y;
+  }
+  String toString() const {
+    return "(" + String(_x) + ", " + String(_y) + ")";
+  }
+  private:
+  const float _x, _y;
+};
+
 class GlobalParameters {
   public:
-  static const float fieldLength {70};
-  static const float fieldDepth {100};
-  static const float spaceBeforeLineSide {5};
-  static const float goalWidth {10};
+    GlobalParameters(
+        float fieldLength,
+        float fieldDepth,
+        float spaceBeforeLineSide,
+        float goalWidth
+      ):
+      _fieldLength(fieldLength),
+      _fieldDepth(fieldDepth),
+      _spaceBeforeLineSide(spaceBeforeLineSide),
+      _goalWidth(goalWidth)
+    {}
+
+    float fieldLength() const {
+      return _fieldLength;
+    }
+    float fieldDepth() const {
+      return _fieldDepth;
+    }
+    float spaceBeforeLineSide() const {
+      return _spaceBeforeLineSide;
+    }
+    float goalWidth() const {
+      return _goalWidth;
+    }
+
+  private:
+    const float _fieldLength;
+    const float _fieldDepth;
+    const float _spaceBeforeLineSide;
+    const float _goalWidth;
+};
+
+class RobotState {
+  public:
+  RobotState(Vector2 ballPos, Vector2 myPos, Vector2 partnerPos): _ballPos(ballPos), _myPos(myPos), _partnerPos(partnerPos) {}
+
+  Vector2 ballPos() const {
+    return _ballPos;
+  }
+  Vector2 myPos() const {
+    return _myPos;
+  }
+  Vector2 partnerPos() const {
+    return _partnerPos;
+  }
+
+  private:
+    const Vector2 _ballPos, _myPos, _partnerPos;
 };
 
 //Required because arduino doesn't support arrays
 class Range {
   public:
-  Range(float from, float to) {
-    _from = from;
-    _to = to;
-  };
+  Range(float from, float to) : _from(from), _to(to) {}
 
-  String toString() {
+  String toString() const {
     return "range from [" + String(_from) + " to " + String(_to) + "]";
-  };
+  }
 
   private:
-  float _from;
-  float _to;
+  const float _from, _to;
 };
 
-Range areboundGetRange() {
-  return breboundGetRange(
-        GlobalParameters::fieldLength /2,
-        GlobalParameters::goalWidth /2,
-        1,
-        1
+Range reboundGetRange(GlobalParameters globalParameters, RobotState robotState) {
+  return reboundGetRange(
+        globalParameters.fieldLength() /2,
+        globalParameters.goalWidth() /2,
+        globalParameters.fieldDepth() - robotState.myPos().y(),
+        (globalParameters.fieldLength() /2) - robotState.myPos().x()
     );
 };
 
-Range breboundGetRange(float halfWidth, float halfWidthGoal, float depthFromRobot, float robotCenterDistance) {
-    return Range(1, 0);
+Range reboundGetRange(float halfWidth, float halfWidthGoal, float depthFromRobot, float robotCenterDistance) {
+  float val = ((2*halfWidth) - robotCenterDistance - halfWidthGoal) / depthFromRobot;
+  Serial.println("before atan : " + String(val, 10));
+  Serial.println("after atan : " + String(atan(val), 10));
+    return Range(
+      atan(((2*halfWidth) - robotCenterDistance - halfWidthGoal) / depthFromRobot)*(PI/180),
+      0
+    );
 };
 
 void setup() {
   // put your setup code here, to run once:
+  GlobalParameters globalParameters = GlobalParameters(5, 10, 0, 1);
+  RobotState robotState = RobotState(
+    Vector2(1, 1),
+    Vector2(2, 2),
+    Vector2(3, 3)
+    );
   Serial.begin(9600);
-  Serial.print(areboundGetRange().toString());
+  Serial.println(reboundGetRange(globalParameters, robotState).toString());
 }
 
 void loop() {
