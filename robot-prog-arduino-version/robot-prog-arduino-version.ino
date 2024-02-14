dee9615b872c0aa518e60a638f8c933dbe0254da
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2024-02-14 19:22:30 +0100

    format

--- robot-prog-arduino-version/robot-prog-arduino-version.ino
+++ robot-prog-arduino-version/robot-prog-arduino-version.ino
@@ -6,8 +6,9 @@
 #define SerialCam Serial1
 
 class Vector2 {
-  public:
-  Vector2(float x, float y) : _x(x), _y(y) {}
+public:
+  Vector2(float x, float y)
+    : _x(x), _y(y) {}
 
   float x() const {
     return _x;
@@ -19,33 +20,31 @@ class Vector2 {
     return "(" + String(_x) + ", " + String(_y) + ")";
   }
 
-  bool operator== (const Vector2 &other) {
+  bool operator==(const Vector2 &other) {
     return (_x == other._x && _y == other._y);
   }
-  bool operator!= (const Vector2 &other) {
+  bool operator!=(const Vector2 &other) {
     return (_x != other._x || _y != other._y);
   }
 
-  private:
+private:
   const float _x, _y;
 };
 
 class Vector2OrError {
-  public:
-  Vector2OrError(String message):
-    _errorMessage(message),
-    _instanceVector2(Vector2(-1, -1)),
-    _hasError(true)
-    {
-      SerialDebug.println("Warning, unraised error :" + message);
-    }
-  
-  Vector2OrError(Vector2 instance):
-    _errorMessage(""),
-    _instanceVector2(instance),
-    _hasError(false)
-    {};
-  
+public:
+  Vector2OrError(String message)
+    : _errorMessage(message),
+      _instanceVector2(Vector2(-1, -1)),
+      _hasError(true) {
+    SerialDebug.println("Warning, unraised error :" + message);
+  }
+
+  Vector2OrError(Vector2 instance)
+    : _errorMessage(""),
+      _instanceVector2(instance),
+      _hasError(false){};
+
   bool isError() const {
     return _hasError;
   }
@@ -66,58 +65,54 @@ class Vector2OrError {
     }
   }
 
-  private:
+private:
   const String _errorMessage;
   const Vector2 _instanceVector2;
   const bool _hasError;
 };
 
 class GlobalParameters {
-  public:
-    GlobalParameters(
-        float fieldLength,
-        float fieldDepth,
-        float spaceBeforeLineSide,
-        float goalWidth
-      ):
-      _fieldLength(fieldLength),
+public:
+  GlobalParameters(
+    float fieldLength,
+    float fieldDepth,
+    float spaceBeforeLineSide,
+    float goalWidth)
+    : _fieldLength(fieldLength),
       _fieldDepth(fieldDepth),
       _spaceBeforeLineSide(spaceBeforeLineSide),
-      _goalWidth(goalWidth)
-    {}
+      _goalWidth(goalWidth) {}
 
-    float fieldLength() const {
-      return _fieldLength;
-    }
-    float fieldDepth() const {
-      return _fieldDepth;
-    }
-    float spaceBeforeLineSide() const {
-      return _spaceBeforeLineSide;
-    }
-    float goalWidth() const {
-      return _goalWidth;
-    }
+  float fieldLength() const {
+    return _fieldLength;
+  }
+  float fieldDepth() const {
+    return _fieldDepth;
+  }
+  float spaceBeforeLineSide() const {
+    return _spaceBeforeLineSide;
+  }
+  float goalWidth() const {
+    return _goalWidth;
+  }
 
-  private:
-    const float _fieldLength;
-    const float _fieldDepth;
-    const float _spaceBeforeLineSide;
-    const float _goalWidth;
+private:
+  const float _fieldLength;
+  const float _fieldDepth;
+  const float _spaceBeforeLineSide;
+  const float _goalWidth;
 };
 
 class RobotState {
-  public:
+public:
   RobotState(
     Vector2 ballPos,
     Vector2 myPos,
-    Vector2 partnerPos
-    ):
-    _ballPos(ballPos),
-    _myPos(myPos),
-    _partnerPos(partnerPos)
-    {}
-  
+    Vector2 partnerPos)
+    : _ballPos(ballPos),
+      _myPos(myPos),
+      _partnerPos(partnerPos) {}
+
   static RobotState fromString(RobotState defaultValues, String values) {
     Vector2OrError b = RobotState::splitLastUpdate(values, 'b');
     Vector2OrError m = RobotState::splitLastUpdate(values, 'm');
@@ -125,8 +120,7 @@ class RobotState {
     return RobotState(
       b.defaultIfError(defaultValues.ballPos()),
       m.defaultIfError(defaultValues.myPos()),
-      p.defaultIfError(defaultValues.partnerPos())
-    );
+      p.defaultIfError(defaultValues.partnerPos()));
   }
 
   static Vector2OrError splitLastUpdate(String values, char charId) {
@@ -168,8 +162,7 @@ class RobotState {
     }
     return Vector2OrError(Vector2(
       x_num.toFloat(),
-      y_num.toFloat()
-    ));
+      y_num.toFloat()));
   }
 
   Vector2 ballPos() const {
@@ -186,43 +179,45 @@ class RobotState {
     return "RobotState (ballPos: " + _ballPos.toString() + " myPos: " + _myPos.toString() + " partnerPos " + _partnerPos.toString() + ")";
   }
 
-  private:
-    const Vector2 _ballPos, _myPos, _partnerPos;
+private:
+  const Vector2 _ballPos, _myPos, _partnerPos;
 };
 
 //Required because arduino doesn't support arrays
 class Range {
-  public:
-  Range(float from, float to) : _from(from), _to(to) {}
+public:
+  Range(float from, float to)
+    : _from(from), _to(to) {}
 
   String toString() const {
     return "range from [" + String(_from) + " to " + String(_to) + "]";
   }
 
-  private:
+private:
   const float _from, _to;
 };
 
 //TODO supprimer la mémorisation de sens en la remplaçant par une détection en temps réel grâce à FG
-enum class Direction {forward, backward, stopped};
+enum class Direction { forward,
+                       backward,
+                       stopped };
 
 //TODO n'utilise pas FG (je ne sais pas comment faire), potentiellemnt remplacer _direction par une détection en directe grâce à FG
 class MotorMov {
-  public:
-    MotorMov(
-      uint8_t pinPWM,
-      uint8_t pinCWCCW,
-      uint8_t pinFG
-    ):_pinPWM(pinPWM),
+public:
+  MotorMov(
+    uint8_t pinPWM,
+    uint8_t pinCWCCW,
+    uint8_t pinFG)
+    : _pinPWM(pinPWM),
       _pinCWCCW(pinCWCCW),
-      _pinFG(pinFG)
-    {
-      pinMode(_pinPWM, OUTPUT);
-      pinMode(_pinCWCCW, OUTPUT);
-      pinMode(_pinFG, INPUT);
-      _direction = Direction::stopped;
-    }
-  
+      _pinFG(pinFG) {
+    pinMode(_pinPWM, OUTPUT);
+    pinMode(_pinCWCCW, OUTPUT);
+    pinMode(_pinFG, INPUT);
+    _direction = Direction::stopped;
+  }
+
   void stop() {
     _pwm(0);
     _direction = Direction::stopped;
@@ -250,74 +245,71 @@ class MotorMov {
     }
   }
 
-  private:
-    const uint8_t _pinPWM;
-    const uint8_t _pinCWCCW;
-    const uint8_t _pinFG;
+private:
+  const uint8_t _pinPWM;
+  const uint8_t _pinCWCCW;
+  const uint8_t _pinFG;
 
-    Direction _direction;//TODO remplacer par détection en direct via fg
+  Direction _direction;  //TODO remplacer par détection en direct via fg
 
-    void _pwm(int value) const {
-      analogWrite(_pinPWM, value);
-    }
+  void _pwm(int value) const {
+    analogWrite(_pinPWM, value);
+  }
 
-    void _cwccw(uint8_t value) const {
-      digitalWrite(_pinCWCCW, value);
-    }
+  void _cwccw(uint8_t value) const {
+    digitalWrite(_pinCWCCW, value);
+  }
 
-    void _fg(uint8_t value) const {
-      digitalWrite(_pinFG, value);
-    }
+  void _fg(uint8_t value) const {
+    digitalWrite(_pinFG, value);
+  }
 };
 
 class Motors {
-  public:
-    Motors(
-      MotorMov frontRight,
-      MotorMov frontLeft,
-      MotorMov backRight,
-      MotorMov backLeft
-    ):
-      _frontRight(frontRight),
+public:
+  Motors(
+    MotorMov frontRight,
+    MotorMov frontLeft,
+    MotorMov backRight,
+    MotorMov backLeft)
+    : _frontRight(frontRight),
       _frontLeft(frontLeft),
       _backRight(backRight),
-      _backLeft(backLeft)
-    {}
+      _backLeft(backLeft) {}
 
-    MotorMov frontRight() const {
-      return _frontRight;
-    }
+  MotorMov frontRight() const {
+    return _frontRight;
+  }
 
-    MotorMov frontLeft() const {
-      return _frontLeft;
-    }
+  MotorMov frontLeft() const {
+    return _frontLeft;
+  }
 
-    MotorMov backRight() const {
-      return _backRight;
-    }
+  MotorMov backRight() const {
+    return _backRight;
+  }
 
-    MotorMov backLeft() const {
-      return _backLeft;
-    }
+  MotorMov backLeft() const {
+    return _backLeft;
+  }
 
-  private:
-    const MotorMov _frontRight;
-    const MotorMov _frontLeft;
-    const MotorMov _backRight;
-    const MotorMov _backLeft;
+private:
+  const MotorMov _frontRight;
+  const MotorMov _frontLeft;
+  const MotorMov _backRight;
+  const MotorMov _backLeft;
 };
 
 Range reboundGetRange(GlobalParameters globalParameters, RobotState robotState) {
   return reboundGetRange(
-        globalParameters.fieldLength() /2,
-        globalParameters.goalWidth() /2,
-        globalParameters.fieldDepth() - robotState.myPos().y(),
-        (globalParameters.fieldLength() /2) - robotState.myPos().x()
-    );
+    globalParameters.fieldLength() / 2,
+    globalParameters.goalWidth() / 2,
+    globalParameters.fieldDepth() - robotState.myPos().y(),
+    (globalParameters.fieldLength() / 2) - robotState.myPos().x());
 };
 
 Range reboundGetRange(float halfWidth, float halfWidthGoal, float depthFromRobot, float robotCenterDistance) {
-  float val = ((2*halfWidth) - robotCenterDistance - halfWidthGoal) / depthFromRobot;
+  float val = ((2 * halfWidth) - robotCenterDistance - halfWidthGoal) / depthFromRobot;
   float valAtan = atan(val);
   // SerialDebug.println("halfWidth L " + String(halfWidth, 10));
   // SerialDebug.println("robotCenterDistance b " + String(robotCenterDistance, 10));
@@ -328,9 +320,8 @@ Range reboundGetRange(float halfWidth, float halfWidthGoal, float depthFromRobot
   // SerialDebug.println("force degree " + String(valAtan*180/PI, 10));
   // SerialDebug.println("force radians " + String(valAtan*PI/180, 10));
   return Range(
-    atan(((2*halfWidth) - robotCenterDistance - halfWidthGoal) / depthFromRobot)*180/PI,
-    0
-  );
+    atan(((2 * halfWidth) - robotCenterDistance - halfWidthGoal) / depthFromRobot) * 180 / PI,
+    0);
 };
 /*
 void setup() {
@@ -352,17 +343,17 @@ void loop() {
 void setup() {
   // put your setup code here, to run once:
   GlobalParameters globalParameters = GlobalParameters(
-    5,    // fieldLength
-    10,   // fieldDepth
-    0,    // spaceBeforeLineSide
-    2     // goalWidth
-    );
+    5,   // fieldLength
+    10,  // fieldDepth
+    0,   // spaceBeforeLineSide
+    2    // goalWidth
+  );
   RobotState robotState = RobotState(
     Vector2(1, 1),  // ballPos
     Vector2(2, 2),  // myPos
     Vector2(3, 3)   // partnerPos
-    );
-    
+  );
+
   SerialDebug.begin(115200);
   SerialCam.begin(115200);
   SerialDebug.println("! started !");
@@ -373,6 +364,6 @@ void loop() {
   RobotState currentState = RobotState::fromString(
     RobotState(Vector2(0, 0), Vector2(0, 0), Vector2(0, 0)),
     SerialCam.readStringUntil("", 100));
-  
+
   SerialDebug.println(currentState.toString());
 }
