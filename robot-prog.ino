149317b0f39fd9f0fe9a9d379ba7a949af66a50b
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2024-03-17 12:29:05 +0100

    add part base code

--- robot-prog.ino
+++ robot-prog.ino
@@ -1,10 +1,22 @@
+#include "lidar.h"
+#include "movements.h"
+#include "states.h"
+#include "strategy.h"
 #include "utilities.h"
 
+const FieldProperties fieldProperties = FieldProperties(
+  5,   // fieldLength
+  10,  // fieldDepth
+  0,   // spaceBeforeLineSide
+  2    // goalWidth
+);
+
 void setup() {
-  Serial.begin(9600);
+  SerialDebug.begin(230400);
+  SerialCam.begin(115200);
+  SerialLidar.begin(230400);
 }
 
 void loop() {
-  Vector2 test = Vector2(1, 1);
-  Serial.println(test.toString());
+  SerialDebug.println("OK!");
 }
