6565aaa62f409cb05cdb51c84d2dc4eca55059a8
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2024-03-17 12:52:17 +0100

    add base currentState

--- robot-prog.ino
+++ robot-prog.ino
@@ -5,11 +5,17 @@
 #include "utilities.h"
 
 const FieldProperties fieldProperties = FieldProperties(
-  5,   // fieldLength
-  10,  // fieldDepth
-  0,   // spaceBeforeLineSide
-  2    // goalWidth
+    5,   // fieldLength
+    10,  // fieldDepth
+    0,   // spaceBeforeLineSide
+    2    // goalWidth
 );
+
+const Motors motors = Motors(
+    MotorMov(1, 2, 3, -55),
+    MotorMov(4, 5, 6, 55),
+    MotorMov(7, 8, 9, -125),
+    MotorMov(10, 11, 12, 125));
 
 void setup() {
   SerialDebug.begin(230400);
@@ -18,5 +24,9 @@ void setup() {
 }
 
 void loop() {
-  SerialDebug.println("OK!");
+  RobotState currentState = RobotState::fromString(
+    RobotState(Vector2(0, 0), Vector2(0, 0), Vector2(0, 0)),
+    SerialCam.readStringUntil("", 100));
+
+  SerialDebug.println(currentState.toString());
 }
