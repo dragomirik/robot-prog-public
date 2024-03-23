b4d5964c20b4274ca244cfd314cf8f3a38cd8409
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2024-03-23 17:30:33 +0100

    continue chooseStrategy

--- strategy.cpp
+++ strategy.cpp
@@ -3,10 +3,19 @@
 
 Vector2 chooseStrategy(FieldProperties fP, RobotState cS) {
   if (!ballIsDetected(fP, cS)) {
-    
+    return slalowingBackwardsStrategy(fP, cS);
   } else if (ballIsCaught(fP, cS)) {
-
+    if (closeToShoot(fP, cS)) {
+      return shootStrategy(fP, cS);
+    } else {
+      return accelerateToGoalStrategy(fP, cS);
+    }
+    accelerateToGoalStrategy(fP, cS);
   } else {
-
+    if (ballInFrontOfRobot(fP, cS)) {
+      return goToBallStrategy(fP, cS);
+    } else {
+      return goToBallAvoidingBallStrategy(fP, cS);
+    }
   }
 }
