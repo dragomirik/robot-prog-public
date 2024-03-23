09cf111dc4f88d42170e8767780c5ae2bd17f6bf
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2024-03-23 17:31:08 +0100

    strategy new names

--- strategy.h
+++ strategy.h
@@ -4,16 +4,17 @@
 #include <Arduino.h>
 #include "states.h"
 
-Vector2 chooseStrategy(FieldProperties fieldProperties, RobotState currentState);
+Vector2 chooseStrategy(FieldProperties fP, RobotState cS);
 
-bool ballIsBeforeRobot(FieldProperties fieldProperties, RobotState currentState);
-bool ballIsDetected(FieldProperties fieldProperties, RobotState currentState);
-bool ballIsCaught(FieldProperties fieldProperties, RobotState currentState);
+bool ballInFrontOfRobot(FieldProperties fP, RobotState cS);
+bool ballIsDetected(FieldProperties fP, RobotState cS);
+bool ballIsCaught(FieldProperties fP, RobotState cS);
+bool closeToShoot(FieldProperties fP, RobotState cS);
 
-Vector2 goToBallStrategy(FieldProperties fieldProperties, RobotState currentState);
-Vector2 goToBallAvoidingBallStrategy(FieldProperties fieldProperties, RobotState currentState);
-Vector2 accelerateToGoalStrategy(FieldProperties fieldProperties, RobotState currentState);
-Vector2 slalowingBackwardsStrategy(FieldProperties fieldProperties, RobotState currentState);
-Vector2 shootStrategy(FieldProperties fieldProperties, RobotState currentState);
+Vector2 goToBallStrategy(FieldProperties fP, RobotState cS);
+Vector2 goToBallAvoidingBallStrategy(FieldProperties fP, RobotState cS);
+Vector2 accelerateToGoalStrategy(FieldProperties fP, RobotState cS);
+Vector2 slalowingBackwardsStrategy(FieldProperties fP, RobotState cS);
+Vector2 shootStrategy(FieldProperties fP, RobotState cS);
 
 #endif
