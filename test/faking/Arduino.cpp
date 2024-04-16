a85cf43a1f6f77db7f96cf4a43b89671b6ddf4e5
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2024-04-16 10:16:56 +0200

    emulation (faking) Arduino

--- test/faking/Arduino.cpp
+++ test/faking/Arduino.cpp
@@ -1,1 +1,15 @@
 #include "Arduino.h"
+
+double sq(double x) {
+  return x * x;
+}
+
+std::string String(double value) {
+  return std::to_string(value);
+}
+
+
+String::String() : std::string() {}
+String::String(const char* s) : std::string(s) {}
+String::String(const std::string& s) : std::string(s) {}
+String::String(double s) : std::string(std::to_string(s)) {}
--- test/faking/Arduino.h
+++ test/faking/Arduino.h
@@ -1,5 +1,17 @@
 #pragma once
 
 #include <string>
+#include <math.h>
 
-typedef std::string String;
+#define PI 3.1415926535897932384626433832795
+#define sqrt std::sqrt
+
+double sq(double x);
+
+class String : public std::string {
+  public:
+    String();
+    String(const char* s);
+    String(const std::string& s);
+    String(double s);
+};
