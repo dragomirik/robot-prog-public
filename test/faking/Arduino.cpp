774b07d88a9da7ea4064fa54c7585f5149d599ae
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2024-04-17 19:37:56 +0200

    improve fake Arduino : add isDigit, String::toFloat, SerialClass, Serialx

--- test/faking/Arduino.cpp
+++ test/faking/Arduino.cpp
@@ -1,15 +1,91 @@
 #include "Arduino.h"
 
+SerialClass Serial;
+SerialClass Serial1;
+SerialClass Serial2;
+SerialClass Serial3;
+SerialClass Serial4;
+SerialClass Serial5;
+SerialClass Serial6;
+SerialClass Serial7;
+SerialClass Serial8;
+
 double sq(double x) {
   return x * x;
 }
 
-std::string String(double value) {
-  return std::to_string(value);
+std::string _removeZeros(std::string str) {
+  size_t dotPos = str.find_last_of('.');
+  if (dotPos != std::string::npos) {
+    size_t nonZeroPos = str.find_last_not_of('0');
+    if (nonZeroPos != std::string::npos && nonZeroPos > dotPos) {
+      return str.substr(0, nonZeroPos + 1);
+    } else if (nonZeroPos != std::string::npos && nonZeroPos == dotPos) {
+      return str.substr(0, nonZeroPos + 2);
+    }
+  }
+  return str;
 }
-
 
 String::String() : std::string() {}
 String::String(const char* s) : std::string(s) {}
 String::String(const std::string& s) : std::string(s) {}
-String::String(double s) : std::string(std::to_string(s)) {}
+String::String(double s) : std::string(_removeZeros(std::to_string(s))) {}
+
+float String::toFloat() {
+  return std::stof(*this);
+}
+
+SerialClass::SerialClass() {}
+
+void SerialClass::begin(int baud) {}
+
+char SerialClass::read() {
+  if (!_incomingData.empty()) {
+    char data = _incomingData.front();
+    _incomingData.pop();
+    return data;
+  } else {
+    return -1;
+  }
+}
+
+void SerialClass::write(char data) {
+  _outgoingData.push(data);
+}
+
+bool SerialClass::available() {
+  return !_incomingData.empty();
+}
+
+bool SerialClass::debugAvailable() {
+  return !_outgoingData.empty();
+}
+
+char SerialClass::debugRead() {
+  if (!_outgoingData.empty()) {
+    char data = _outgoingData.front();
+    _outgoingData.pop();
+    return data;
+  } else {
+    return -1;
+  }
+}
+
+void SerialClass::debugWrite(char data) {
+  _incomingData.push(data);
+}
+
+void SerialClass::println(const std::string& str) {
+  for (char c : str) {
+    write(c);
+  }
+  write('\n');
+}
+
+void SerialClass::debugPrintln(const std::string& str) {
+  for (char c : str) {
+    debugWrite(c);
+  }
+  debugWrite('\n');
+}
--- test/faking/Arduino.h
+++ test/faking/Arduino.h
@@ -1,20 +1,58 @@
-#pragma once
+#ifndef FAKE_ARDUINO_H
+#define FAKE_ARDUINO_H
 
-#include <string>
 #include <math.h>
+
+#include <cctype>
+#include <queue>
+#include <string>
 
 #define PI 3.1415926535897932384626433832795
 #define DEG_TO_RAD 0.017453292519943295769236907684886
 #define RAD_TO_DEG 57.295779513082320876798154814105
 
 #define sqrt std::sqrt
+#define isDigit std::isdigit
 
 double sq(double x);
 
+std::string _removeZeros(std::string str);
 class String : public std::string {
-  public:
-    String();
-    String(const char* s);
-    String(const std::string& s);
-    String(double s);
+ public:
+  String();
+  String(const char* s);
+  String(const std::string& s);
+  String(double s);
+
+  float toFloat();
 };
+
+class SerialClass {
+ private:
+  std::queue<char> _incomingData;
+  std::queue<char> _outgoingData;
+
+ public:
+  SerialClass();
+  void begin(int baud);
+  char read();
+  void write(char data);
+  bool available();
+  void println(const std::string& str);
+  char debugRead();
+  void debugWrite(char data);
+  bool debugAvailable();
+  void debugPrintln(const std::string& str);
+};
+
+extern SerialClass Serial;
+extern SerialClass Serial1;
+extern SerialClass Serial2;
+extern SerialClass Serial3;
+extern SerialClass Serial4;
+extern SerialClass Serial5;
+extern SerialClass Serial6;
+extern SerialClass Serial7;
+extern SerialClass Serial8;
+
+#endif
